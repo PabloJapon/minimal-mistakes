643d8f0dddc251413567cacdf38a47e72c9a11e5
AppResta <159771503+AppResta@users.noreply.github.com>
2024-04-16 16:28:48 +0200

    Update stripe_checkout.js

--- _pages/stripe_checkout.js
+++ _pages/stripe_checkout.js
@@ -1,5 +1,7 @@
 var stripe = Stripe('pk_test_51OmfAYE2UvP4xcDs92nWGG93clovJ2N6OBjuvPv9k26lrUnU0VDdS4ra32km006KbVhlHGygobi4SQpTbpBTeyGa00FwesDfwo');
 
+console.log('Redirigiendo a Stripe Checkout...');
+
 stripe.redirectToCheckout({
   sessionId: 'id_de_la_sesion_de_pago',
   successUrl: 'pago-exitoso.html',
