58d6e64cb319172bb5182af03dcf0c2e44cd329b
AppResta <159771503+AppResta@users.noreply.github.com>
2024-04-16 16:19:41 +0200

    Update stripe_checkout.js

--- _pages/stripe_checkout.js
+++ _pages/stripe_checkout.js
@@ -1,9 +1,9 @@
-var stripe = Stripe('tu_clave_publica_de_stripe');
+var stripe = Stripe('pk_test_51OmfAYE2UvP4xcDs92nWGG93clovJ2N6OBjuvPv9k26lrUnU0VDdS4ra32km006KbVhlHGygobi4SQpTbpBTeyGa00FwesDfwo');
 
 stripe.redirectToCheckout({
   sessionId: 'id_de_la_sesion_de_pago',
-  successUrl: 'https://tupaginaweb.com/pagina-de-confirmacion',
-  cancelUrl: 'https://tupaginaweb.com/pago-cancelado',
+  successUrl: 'pago-exitoso.html',
+  cancelUrl: 'pago-cancelado.html',
 }).then(function (result) {
   // Manejar errores, si los hay
 });
