40ff93be97125c61835e5f0fed6f189fe949dc34
AppResta <159771503+AppResta@users.noreply.github.com>
2024-04-22 09:46:41 +0200

    Update stripe_checkout.js

--- _pages/stripe_checkout.js
+++ _pages/stripe_checkout.js
@@ -1,11 +1,15 @@
+console.log('Script execution started.');
+
 var stripe = Stripe('pk_test_51OmfAYE2UvP4xcDs92nWGG93clovJ2N6OBjuvPv9k26lrUnU0VDdS4ra32km006KbVhlHGygobi4SQpTbpBTeyGa00FwesDfwo');
 
-console.log('Redirigiendo a Stripe Checkout...');
+console.log('Stripe object initialized.');
 
 stripe.redirectToCheckout({
   sessionId: 'id_de_la_sesion_de_pago',
   successUrl: 'pago-exitoso.html',
   cancelUrl: 'pago-cancelado.html',
 }).then(function (result) {
-  // Manejar errores, si los hay
+  console.log('Redirect to Stripe Checkout initiated.');
+}).catch(function (error) {
+  console.error('Error redirecting to Stripe Checkout:', error);
 });
