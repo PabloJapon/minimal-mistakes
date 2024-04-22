27d6c243904b9f0a6ad10bf9bbc8e91001a656e9
AppResta <159771503+AppResta@users.noreply.github.com>
2024-04-22 11:52:28 +0200

    Update getSubscription.js

--- functions1/getSubscription.js
+++ functions1/getSubscription.js
@@ -1,16 +1,20 @@
-// functions1/getSubscription.js
+// functions/getSubscription.js
 
 // Import the Stripe SDK
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // Handler function for the serverless function
 exports.handler = async (event) => {
+  console.log('Function invoked with event:', event);
+
   // Parse the incoming request body as JSON
   const requestBody = JSON.parse(event.body);
+  console.log('Request body:', requestBody);
 
   try {
     // Retrieve the customer ID from the request body
     const customerId = requestBody.customerId;
+    console.log('Customer ID:', customerId);
 
     // Retrieve the customer's subscriptions from Stripe
     const subscriptions = await stripe.subscriptions.list({
@@ -18,16 +22,19 @@ exports.handler = async (event) => {
       status: 'active',
       limit: 1, // Limit to one active subscription
     });
+    console.log('Subscriptions:', subscriptions);
 
     // If the customer has an active subscription, return the plan name
     if (subscriptions && subscriptions.data && subscriptions.data.length > 0) {
       const planName = subscriptions.data[0].plan.nickname; // Change 'nickname' to the appropriate field containing the plan name
+      console.log('Plan name:', planName);
       return {
         statusCode: 200,
         body: JSON.stringify({ planName }),
       };
     } else {
       // If the customer does not have an active subscription, return an error
+      console.log('No active subscription found');
       return {
         statusCode: 404,
         body: JSON.stringify({ error: 'No active subscription found' }),
@@ -35,9 +42,11 @@ exports.handler = async (event) => {
     }
   } catch (error) {
     // If an error occurs, return an error response
+    console.error('Error:', error.message);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
     };
   }
 };
+
