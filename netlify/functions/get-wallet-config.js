50e2251df4cfc0bec96d501acf731295f0d35ad1
Zanahorio <forbiddenplaces96@gmail.com>
2024-04-23 11:34:32 +0200

    no message

--- netlify/functions/get-wallet-config.js
+++ netlify/functions/get-wallet-config.js
@@ -2,7 +2,11 @@
 
 exports.handler = async (event, context) => {
     try {
-        const response = await fetch('https://merchant-ui-api.stripe.com/elements/wallet-config');
+        const response = await fetch('https://merchant-ui-api.stripe.com/elements/wallet-config', {
+            headers: {
+                'Authorization': 'Bearer pk_test_51OmfAYE2UvP4xcDs92nWGG93clovJ2N6OBjuvPv9k26lrUnU0VDdS4ra32km006KbVhlHGygobi4SQpTbpBTeyGa00FwesDfwo' // Replace 'YOUR_PUBLISHABLE_KEY' with your actual Stripe publishable key
+            }
+        });
         const data = await response.json();
 
         return {
