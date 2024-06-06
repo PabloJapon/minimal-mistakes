e6c95d4e61d2824b846560a565b978af64cbbfd6
Zanahorio <forbiddenplaces96@gmail.com>
2024-06-06 20:53:21 +0200

    no message

--- netlify/functions/verify-identity.js
+++ netlify/functions/verify-identity.js
@@ -1,29 +1,35 @@
 exports.handler = async (event, context) => {
-    // Use dynamic import for node-fetch
     const fetch = (await import('node-fetch')).default;
     
-    const { email, password } = JSON.parse(event.body);
-    
     try {
+      const { email, password } = JSON.parse(event.body);
+      console.log(`Received email: ${email}`);
+      console.log(`Received password: ${password}`);
+  
       const response = await fetch('https://gastrali.netlify.app/.netlify/identity/token', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ grant_type: 'password', username: email, password: password })
       });
   
+      const responseBody = await response.text();
+      console.log(`Response status: ${response.status}`);
+      console.log(`Response body: ${responseBody}`);
+  
       if (!response.ok) {
         return {
           statusCode: response.status,
-          body: JSON.stringify({ error: 'Invalid credentials' }),
+          body: JSON.stringify({ error: responseBody }),
         };
       }
   
-      const data = await response.json();
+      const data = JSON.parse(responseBody);
       return {
         statusCode: 200,
         body: JSON.stringify({ token: data.access_token }),
       };
     } catch (error) {
+      console.error('Error:', error);
       return {
         statusCode: 500,
         body: JSON.stringify({ error: 'Internal Server Error' }),
