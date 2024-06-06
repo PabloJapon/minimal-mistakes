e09be80cd0c5517c927d33c5241aac477a7b55dd
Zanahorio <forbiddenplaces96@gmail.com>
2024-06-06 20:49:16 +0200

    no message

--- netlify/functions/verify-identity.js
+++ netlify/functions/verify-identity.js
@@ -1,32 +1,33 @@
-// functions/verify-identity.js
-const fetch = require('node-fetch');
-
 exports.handler = async (event, context) => {
-  const { email, password } = JSON.parse(event.body);
+    // Use dynamic import for node-fetch
+    const fetch = (await import('node-fetch')).default;
+    
+    const { email, password } = JSON.parse(event.body);
+    
+    try {
+      const response = await fetch('https://gastrali.netlify.app/.netlify/identity/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ grant_type: 'password', username: email, password: password })
+      });
   
-  try {
-    const response = await fetch('https://gastrali.netlify.app/.netlify/identity/token', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ grant_type: 'password', username: email, password: password })
-    });
-
-    if (!response.ok) {
+      if (!response.ok) {
+        return {
+          statusCode: response.status,
+          body: JSON.stringify({ error: 'Invalid credentials' }),
+        };
+      }
+  
+      const data = await response.json();
       return {
-        statusCode: response.status,
-        body: JSON.stringify({ error: 'Invalid credentials' }),
+        statusCode: 200,
+        body: JSON.stringify({ token: data.access_token }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' }),
       };
     }
-
-    const data = await response.json();
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ token: data.access_token }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
-  }
-};
+  };
+  
