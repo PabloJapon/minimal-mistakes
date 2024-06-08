2316b665087034f7fa9f626997ee583e376ea166
Zanahorio <forbiddenplaces96@gmail.com>
2024-06-08 17:33:11 +0200

    no message

---
+++ _pages/verify-account.md
@@ -0,0 +1,18 @@
+---
+title: "verify-account"
+permalink: /verifyAccount/
+layout: splash
+---
+
+<script>
+  fetch('https://gastrali.netlify.app/.netlify/functions/verificar-sesion', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ value: 1 })
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+</script>
--- netlify/functions/verificar-sesion.js
+++ netlify/functions/verificar-sesion.js
@@ -1,28 +1,9 @@
 exports.handler = async (event, context) => {
-    // Verificar si hay un usuario autenticado
-    const { user } = context.clientContext;
+    const data = JSON.parse(event.body);
+    console.log("Received data:", data);
 
-    if (!user) {
-        return {
-            statusCode: 401,
-            body: JSON.stringify({ error: "Usuario no autenticado" })
-        };
-    }
-
-    try {
-        // Acceder a los detalles del usuario
-        const username = user.user_metadata.full_name || user.email;
-        const subscriptionPlan = user.user_metadata.subscription_plan;
-
-        // Devolver los detalles del usuario en la respuesta
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ username, subscriptionPlan })
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: "Error al procesar la solicitud" })
-        };
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Data received successfully', receivedData: data })
+    };
 };
