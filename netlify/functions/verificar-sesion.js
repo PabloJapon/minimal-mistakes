c896db0ecf8e6588de78c4af16f2d387e15b9e8b
Zanahorio <forbiddenplaces96@gmail.com>
2024-06-10 11:34:44 +0200

    no message

--- netlify/functions/verificar-sesion.js
+++ netlify/functions/verificar-sesion.js
@@ -1,19 +1,6 @@
 exports.handler = async (event, context) => {
-    const { user } = context.clientContext;
-
-    console.log("User:", user); // Log the user object for debugging
-
-    if (user) {
-        console.log("User is logged in"); // Log that user is logged in
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ isLoggedIn: true })
-        };
-    } else {
-        console.log("User is not logged in"); // Log that user is not logged in
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ isLoggedIn: false })
-        };
-    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ hello })
+    };
 };
