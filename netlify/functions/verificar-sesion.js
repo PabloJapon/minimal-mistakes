7a68cd6978c4e392ec90185e5a6f8bdc8ca7aafa
Zanahorio <forbiddenplaces96@gmail.com>
2024-06-10 10:18:12 +0200

    no message

--- netlify/functions/verificar-sesion.js
+++ netlify/functions/verificar-sesion.js
@@ -1,9 +1,6 @@
 exports.handler = async (event, context) => {
-    const data = JSON.parse(event.body);
-    console.log("Received data:", data);
-
     return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Data received successfully', receivedData: data })
+      statusCode: 200,
+      body: JSON.stringify({ message: "hello" }),
     };
-};
+  };
