c9b69832fe1e8f9ba2bd52d188b5970ea9da17d8
Zanahorio <forbiddenplaces96@gmail.com>
2024-09-17 11:32:31 +0200

    no message

--- netlify/functions/verificar-sesion.js
+++ netlify/functions/verificar-sesion.js
@@ -8,31 +8,32 @@ exports.handler = async (event, context) => {
             const requestBody = JSON.parse(event.body);
             console.log("Request body:", requestBody);
 
+            // Store the input data from the POST request
             storedInputData = {
                 message: requestBody.message,
                 subscription_plan: requestBody.subscription_plan,
                 id: requestBody.id // Store the ID from the POST request
-            }; // Store the inputData from the POST request
+            }; 
             console.log("Input data stored:", storedInputData);
 
+            // Prepare response data
             const responseData = { 
                 message: "Data received successfully", 
-                inputData: storedInputData // Include the stored inputData in the response
+                inputData: storedInputData
             };
-            console.log("Response data:", responseData);
 
             return {
                 statusCode: 200,
                 body: JSON.stringify(responseData)
             };
         } else if (event.httpMethod === "GET") {
-            // Handle GET request (sending data to Unity)
+            // Handle GET request (return stored data)
             if (storedInputData !== null) {
                 return {
                     statusCode: 200,
                     body: JSON.stringify({ 
                         message: "Data retrieved successfully from previous POST request",
-                        inputData: storedInputData // Include the stored inputData in the response to the GET request
+                        inputData: storedInputData
                     })
                 };
             } else {
