17324e78a6c21d00e2e98491c65ceb7ba3caaf91
PabloJapon <159771503+PabloJapon@users.noreply.github.com>
2025-03-14 14:16:33 +0100

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -278,6 +278,46 @@ exports.handler = async (event, context) => {
       };
     }
 
+    if (body.action === 'get_productId') {
+      const customerEmail = body.email;
+
+      const customers = await stripe.customers.list({ email: customerEmail, limit: 1 });
+      const customer = customers.data[0];
+
+      if (!customer) {
+        return {
+          statusCode: 400,
+          headers,  // Include CORS headers
+          body: JSON.stringify({ error: 'Customer not found' })
+        };
+      }
+
+      const subscriptions = await stripe.subscriptions.list({
+        customer: customer.id,
+        status: 'active',
+        limit: 1
+      });
+
+      if (subscriptions.data.length === 0) {
+        return {
+          statusCode: 400,
+          headers,  // Include CORS headers
+          body: JSON.stringify({ error: 'No active subscription found for the customer' })
+        };
+      }
+
+      const subscription = subscriptions.data[0];
+
+      // Get the product ID from the subscription
+      const productId = subscription.items.data[0].price.product;
+
+      return {
+        statusCode: 200,
+        headers,  // Include CORS headers
+        body: JSON.stringify({ productId })
+      };
+    }
+
     if (body.action === 'get_invoices') {
       const customerEmail = body.email;
 
